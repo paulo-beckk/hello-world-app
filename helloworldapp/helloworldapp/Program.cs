a2e7e0de3f5520a16fff355fad887d6b49d9ad2f
paulo-beckk <paulo.beckk@gmail.com>
2020-03-26 02:22:04 -0300

    First deploy

---
+++ helloworldapp/helloworldapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloworldapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
